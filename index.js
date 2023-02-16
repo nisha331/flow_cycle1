d3c7ae46100a7f685d4a77a227d73158aec25fc3
smitra1609 <smitra0916@gmail.com>
2023-02-16 19:07:55 +0530

    added external jscript

---
+++ index.js
@@ -0,0 +1,119 @@
+//ENTER key
+document.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    Calculator();
+  }
+});
+function toggle(ele) {
+  var cont = document.getElementById("cont");
+  cont.style.display = cont.style.display == "none" ? "block" : "none";
+}
+function Calculator() {
+  console.log("In");
+  var userinput = document.getElementById("Day").value;
+  var y1 = parseInt(userinput.slice(0, 4));
+  var m1 = parseInt(userinput.slice(5, 7));
+  var d1 = parseInt(userinput.slice(8, 10));
+  var y2 = "";
+  var m2 = "";
+  var d2 = "";
+  var sum = d1 + 28;
+  var monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  //wrong DATE
+  if (userinput == null || userinput == "") {
+    document.getElementById("result").innerHTML = "**Choose a date please!";
+    return false;
+  } else if ((y1 % 4 == 0 && y1 % 100 == 0 && y1 % 400 != 0) || y1 % 4 != 0) {
+    // console.log("non leap");
+    if (m1 == 02) {
+      //FEB
+      m2 = m1 + 1;
+      d2 = d1;
+      y2 = y1;
+    } else if (m1 == 04 || m1 == 06 || m1 == 09 || m1 == 11) {
+      //30days
+      m2 = m1 + 1;
+      y2 = y1;
+      if (sum > 30) {
+        d2 = sum - 30;
+      } else {
+        d2 = sum;
+      }
+    } else if (m1 == 12) {
+      //DEC
+      m2 = 01;
+      y2 = y1 + 1;
+      if (sum > 31) {
+        d2 = sum - 31;
+      } else {
+        d2 = sum;
+      }
+    } else {
+      //31days
+      m2 = m1 + 1;
+      y2 = y1;
+      if (sum > 31) {
+        d2 = sum - 31;
+      } else {
+        d2 = sum;
+      }
+    }
+    document.getElementById("result").innerHTML =
+      d2 + " " + monthNames[m2 - 1] + " " + y2;
+  } else {
+    // console.log("leap");
+    if (m1 == 02) {
+      //FEB
+      m2 = m1 + 1;
+      y2 = y1;
+      if (sum > 29) {
+        d2 = sum - 29;
+      } else {
+        d2 = sum;
+      }
+    } else if (m1 == 04 || m1 == 06 || m1 == 09 || m1 == 11) {
+      //30days
+      m2 = m1 + 1;
+      y2 = y1;
+      if (sum > 30) {
+        d2 = sum - 30;
+      } else {
+        d2 = sum;
+      }
+    } else if (m1 == 12) {
+      //DEC
+      m2 = 01;
+      y2 = y1 + 1;
+      if (sum > 31) {
+        d2 = sum - 31;
+      } else {
+        d2 = sum;
+      }
+    } else {
+      //31days
+      m2 = m1 + 1;
+      y2 = y1;
+      if (sum > 31) {
+        d2 = sum - 31;
+      } else {
+        d2 = sum;
+      }
+    }
+    document.getElementById("result").innerHTML =
+      d2 + " " + monthNames[m2 - 1] + " " + y2;
+  }
+}
