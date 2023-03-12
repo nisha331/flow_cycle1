65351abca735b79934f8f692334a12c469b1b82e
Pooranjoy Bhattacharya <pooranjoyb2016@gmail.com>
2023-03-12 23:49:26 +0530

    Changed relative locations of files (#112)

---
+++ src/js/question.js
@@ -0,0 +1,119 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "On a scale of 1 to 10, how stressful is your day?",
+    answer: "A. Less than 5",
+    options: [
+      "A. Less than 5",
+      "B. More than 5"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Are you noticing sudden weight gain?",
+    answer: "B. No, I work really hard to manage it",
+    options: [
+      "A. Oh yes",
+      "B. No, I work really hard to manage it",
+    ]
+  },
+  {
+    numb: 3,
+    question: "Do you use hormonal birth control pills?",
+    answer: "A. Yeah",
+    options: [
+      "A. Yeah",
+      "B. No",
+    ]
+  },
+  {
+    numb: 4,
+    question: "	How often do you exercise?",
+    answer: "B. Oh I’m a fitness freak",
+    options: [
+      "A. I’m not that regular",
+      "B. Oh I’m a fitness freak",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Do your periods often go missing?",
+    answer: "B. Thankfully, no",
+    options: [
+      "A. Sometimes",
+      "B. Thankfully, no",
+    ]
+  },
+
+  {
+    numb: 6,
+    question: "How long do your periods usually last?",
+    answer: "A. 4 to 7 days",
+    options: [
+      "A. 4 to 7 days",
+      "B. More than 7 days",
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "What about cramps – how bad do they get?",
+    answer: "A.  Mild cramps right before it arrives and some pain during the first few days",
+    options: [
+      "A.   Mild cramps right before it arrives and some pain during the first few days",
+      "B. SERIOUS pain, often the can’t get out of bed kind",
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Do you have a sweet tooth?",
+    answer: "A. Yes, big time",
+    options: [
+      "A. Yes, big time",
+      "B. Not really",
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "Do you have pre-existing health issues?",
+    answer: "B. No",
+    options: [
+      "A. Yes",
+      "B. No",
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "How often do you get your period?",
+    answer: "B. Like clockwork, every 28 to 30 days",
+    options: [
+      "A.  Sometimes it comes a week early or a week later",
+      "B. Like clockwork, every 28 to 30 days",
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "	What’s your sleep cycle like?",
+    answer: "B. I sleep 7 to 8 hours a day",
+    options: [
+      "A. It’s very disrupted",
+      "B. I sleep 7 to 8 hours a day",
+    ]
+  },
+
+  {
+    numb: 12,
+    question: " How long is your luteal phase?",
+    answer: "B. 14 days, all time, every time. I have used it accurately to conceive and have been successful",
+    options: [
+      "A. My period is so irregular I never know",
+      "B. 14 days, all time, every time. I have used it accurately to conceive and have been successful",
+    ]
+  },
+
+];
